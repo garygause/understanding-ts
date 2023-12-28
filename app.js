fe84d8d3c0b4ea5b82842f6fa1e477222ba5e52a
Gary Gause <garygause@gmail.com>
2023-12-28 10:35:12 -0800

    lesson 28 - callbacks

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ function add(n1, n2) {
 function printResult(num) {
     console.log('Result: ' + num);
 }
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
+// function type
 var combineValues;
 combineValues = add;
 console.log(combineValues(8, 5));
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+});
--- app.ts
+++ app.ts
@@ -6,9 +6,18 @@ function printResult(num: number): void {
   console.log('Result: ' + num);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
 // function type
 let combineValues: (a: number, b: number) => number;
 
 combineValues = add;
 
 console.log(combineValues(8, 5));
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+});
