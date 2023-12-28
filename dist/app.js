62f2381fadf9d635607d5eb2837ca23a7668998e
Gary Gause <garygause@gmail.com>
2023-12-28 13:52:33 -0800

    setup tsconfig

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,4 @@
+"use strict";
 function add(n1, n2) {
     return n1 + n2;
 }
@@ -5,13 +6,13 @@ function printResult(num) {
     console.log('Result: ' + num);
 }
 function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
+    const result = n1 + n2;
     cb(result);
 }
 // function type
-var combineValues;
+let combineValues;
 combineValues = add;
 console.log(combineValues(8, 5));
-addAndHandle(10, 20, function (result) {
+addAndHandle(10, 20, (result) => {
     console.log(result);
 });
