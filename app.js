199e7b0e5eba73298428276b9a0aebf710a6652c
Gary Gause <garygause@gmail.com>
2023-12-28 09:48:23 -0800

    lesson 25 - type alises

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function combine(input1, input2) {
+function combine(input1, input2, resultType) {
     var result;
     if (typeof input1 === 'number' && typeof input2 === 'number') {
         result = input1 + input2;
@@ -6,7 +6,13 @@ function combine(input1, input2) {
     else {
         result = input1.toString() + input2.toString();
     }
+    if (resultType === 'as-number') {
+        return +result;
+    }
+    else {
+        return result.toString();
+    }
     return result;
 }
-var comnbinedValues = combine(30, 26);
+var comnbinedValues = combine(30, 26, 'as-number');
 console.log(comnbinedValues);
--- app.ts
+++ app.ts
@@ -1,12 +1,24 @@
-function combine(input1: number | string, input2: number | string) {
+type Combinable = number | string;
+type ConversionDescriptor = 'as-number' | 'as-text';
+
+function combine(
+  input1: Combinable,
+  input2: Combinable,
+  resultType: ConversionDescriptor
+) {
   let result;
   if (typeof input1 === 'number' && typeof input2 === 'number') {
     result = input1 + input2;
   } else {
     result = input1.toString() + input2.toString();
   }
+  if (resultType === 'as-number') {
+    return +result;
+  } else {
+    return result.toString();
+  }
   return result;
 }
 
-const comnbinedValues = combine(30, 26);
+const comnbinedValues = combine(30, 26, 'as-number');
 console.log(comnbinedValues);
