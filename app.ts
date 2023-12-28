540c4e711a2468770f947cc938262ab7dcae5f97
Gary Gause <garygause@gmail.com>
2023-12-28 10:21:12 -0800

    lesson 26 - function return types

--- app.ts
+++ app.ts
@@ -1,24 +1,7 @@
-type Combinable = number | string;
-type ConversionDescriptor = 'as-number' | 'as-text';
-
-function combine(
-  input1: Combinable,
-  input2: Combinable,
-  resultType: ConversionDescriptor
-) {
-  let result;
-  if (typeof input1 === 'number' && typeof input2 === 'number') {
-    result = input1 + input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-  if (resultType === 'as-number') {
-    return +result;
-  } else {
-    return result.toString();
-  }
-  return result;
+function add(n1: number, n2: number): number {
+  return n1 + n2;
 }
 
-const comnbinedValues = combine(30, 26, 'as-number');
-console.log(comnbinedValues);
+function printResult(num: number): void {
+  console.log('Result: ' + num);
+}
