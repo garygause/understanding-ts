034c8330e402c5c05e0be6cf5ccf1159d6a350f8
Gary Gause <garygause@gmail.com>
2023-12-28 10:27:26 -0800

    lesson 27 - function types

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
-function combine(input1, input2, resultType) {
-    var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    if (resultType === 'as-number') {
-        return +result;
-    }
-    else {
-        return result.toString();
-    }
-    return result;
+function add(n1, n2) {
+    return n1 + n2;
 }
-var comnbinedValues = combine(30, 26, 'as-number');
-console.log(comnbinedValues);
+function printResult(num) {
+    console.log('Result: ' + num);
+}
+var combineValues;
+combineValues = add;
+console.log(combineValues(8, 5));
--- app.ts
+++ app.ts
@@ -5,3 +5,10 @@ function add(n1: number, n2: number): number {
 function printResult(num: number): void {
   console.log('Result: ' + num);
 }
+
+// function type
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+
+console.log(combineValues(8, 5));
